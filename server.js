200b27c0bdd8f85a44ab3c344b744cfac5999985
Sakshi2k <sakshi2k@yahoo.co.in>
2020-05-31 15:31:34 +0530

    git-ignore

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // MONGO DB CONNECTION
-mongoose.connect('mongodb://localhost:27017/schoolDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/schoolDB', {useNewUrlParser: true, useUnifiedTopology: true},
+     (err) => {
+        if(!err) console.log("Connected to DB"); 
+     });
 
-const schoolSchema = {
+// Mongoose schema for school.
+const schoolSchema = mongoose.Schema({
     schoolParEnt : String ,
     name : String,
     year : Number,
@@ -23,45 +27,49 @@ const schoolSchema = {
     state : String,
     district : String,
     postcode : Number
-}
+});
 
+// Mongoose Model
 const School = mongoose.model("School", schoolSchema);
 
-app.get("/", (req,res) => {
-    res.render("form", {sucessMessage : ""});
-})
+// REST implementation at home route.
+app.route("/")
 
-app.post("/", (req, res) => {
-    const {schoolParEnt, name, year, noOfStudents, address,
-    city, state, district, postcode} = req.body;
-    
-    const newSchool = new School({
-        schoolParEnt : schoolParEnt ,
-        name : name,
-        year : year,
-        noOfStudents : noOfStudents,
-        address : address,
-        city : city,
-        state : state,
-        district : district,
-        postcode : postcode
-    });
+    .get((req,res) => {
+        res.render("form", {sucessMessage : ""});
+    })
 
-    try{
-        School.findOne({name : name}, (err, foundSchool) => { 
-            if(!err){
-                if(foundSchool){
-                    res.send("School already registered, Try another ?");
-                } else {
-                    newSchool.save();    
-                    res.send("Successfully registered");
+    .post((req, res) => {
+        const {schoolParEnt, name, year, noOfStudents, address,
+        city, state, district, postcode} = req.body;
+        
+        const newSchool = new School({
+            schoolParEnt : schoolParEnt ,
+            name : name,
+            year : year,
+            noOfStudents : noOfStudents,
+            address : address,
+            city : city,
+            state : state,
+            district : district,
+            postcode : postcode
+        });
+
+        try{
+            School.findOne({name : name}, (err, foundSchool) => { 
+                if(!err){
+                    if(foundSchool){
+                        res.send("School already registered, Try another ?");
+                    } else {
+                        newSchool.save();    
+                        res.send("Successfully registered");
+                    }
                 }
-            }
-        })
-    } catch(err){
-        res.send(err);
-    }
-})
+            })
+        } catch(err){
+            res.send(err);
+        }
+    });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
